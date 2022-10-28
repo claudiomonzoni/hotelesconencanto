3ca05fdd2075f8b1b67a8cc2220947e2585a2408
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-10-27 20:14:10 -0500

    ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://hotelesconencanto.com.mx',
+  integrations: [mdx(), sitemap(), robotsTxt({sitemap: true,})],
 });
