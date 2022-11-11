9eb342a66e12c0ca7a3e258f5665bf45e084969a
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-11-11 17:53:35 -0600

    a traducir se a dicho

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hotelesconencanto.com.mx',
-  integrations: [mdx(), sitemap(), robotsTxt({sitemap: true,})],
+  integrations: [ mdx(), sitemap(), robotsTxt({sitemap: true,}), astroI18next()],
 });
