adbdeb3211384cb69476ecbedddfd6367b88e176
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-11-10 09:57:15 -0600

    agregando componente de noticias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import astroI18next from "astro-i18next";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hotelesconencanto.com.mx',
-  integrations: [ mdx(), sitemap(), robotsTxt({sitemap: true,}), astroI18next()],
+  integrations: [mdx(), sitemap(), robotsTxt({
+    sitemap: true
+  }), astroI18next(), react()]
   // experimental: {
   //   assets: true
   //  }
---
+++ src/components/Notas.jsx
@@ -0,0 +1,8 @@
+
+export default function Notas() {
+  return (
+    <div>
+        <h1> Noticias</h1>
+        </div>
+  )
+}
